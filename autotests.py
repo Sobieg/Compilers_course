50b6b44f2d3d67a02bbed652efa9429909e13ae1
Sobieg <sobiegrus@gmail.com>
2019-04-07 17:36:31 +0300

    Update autotests.py

--- autotests.py
+++ autotests.py
@@ -5,13 +5,11 @@ import subprocess
 prog = "./a.out"
 files = glob("tests/*.lua")
 
-for arg in files:
-	args = ["./a.out"]
-	popen = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-	testfile = open(arg, "r")
-	lines = testfile.readlines()
-	for x in lines:
-		popen.stdin.write(bytes(x, 'utf-8'))
-	popen.wait()
-	output = popen.stdout.read();
-	print(output)
+
+args = ["ping", "-t", "4", "google.com"]
+proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+
+data = proc.communicate()
+for line in data:
+	if (line):
+		print(line.decode("utf-8"))
